e57a21cbec66d52f378ab6ca35596425a1e4e4e2
calvin ho <calvinho1214@gmail.com>
2018-11-06 12:08:49 -0500

    prompt

--- bash.js
+++ bash.js
@@ -1,6 +1,6 @@
-process.stnout.write('prompt > ');
+process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data =>){
+process.stdin.on('data', (data) =>{
   const cmd = data.toString().trim();
 
   process.stdout.write('You typed: ' + cmd);
